787da03b26fdb584d394c6be24eb8f59be9dded0
sukanda237 <sukandawiguna@mail.unpas.ac.id>
2024-12-16 08:41:11 +0700

    menambahkan 3 fungsi pada m_contoh.php

--- models/M_contoh.php
+++ models/M_contoh.php
@@ -1,3 +1,21 @@
 <?php
 
-class m_contoh {}
+class m_contoh
+{
+    // bikin 3 fungsi, tampil, edit, hapus
+
+    public function tampil()
+    {
+        // fungsi untuk menampilkan data dari database 
+    }
+
+    function edit()
+    {
+        // fungsi untuk edit data dari database 
+    }
+
+    function hapus()
+    {
+        // fungsi untuk menghapus data dari database 
+    }
+}
